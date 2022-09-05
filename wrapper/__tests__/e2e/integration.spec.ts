9bc3097ee81eea138825540a894d49c1a18bf0af
fetsorn <fetsorn@gmail.com>
2022-09-05 18:42:13 +0400

    add implementations

--- wrapper/__tests__/e2e/integration.spec.ts
+++ wrapper/__tests__/e2e/integration.spec.ts
@@ -13,7 +13,7 @@ import { abi, bytecode } from "./GnosisSafeProxyFactory";
 
 jest.setTimeout(500000);
 
-describe("SimpleStorage", () => {
+describe("ProxyFactory", () => {
   const CONNECTION = { networkNameOrChainId: "testnet" };
 
   let client: PolywrapClient;
@@ -38,8 +38,7 @@ describe("SimpleStorage", () => {
     await stopTestEnvironment();
   });
 
-  it("sanity", async () => {
-    // Deploy contract
+  it("createProxy", async () => {
 
      const deployContractResponse = await App.Ethereum_Module.deployContract(
        {
@@ -54,16 +53,13 @@ describe("SimpleStorage", () => {
 
     const contractAddress = deployContractResponse.data as string;
 
-    console.log(contractAddress)
-
     expect(deployContractResponse).toBeTruthy();
     expect(deployContractResponse.error).toBeFalsy();
     expect(deployContractResponse.data).toBeTruthy();
 
     const initCode = "0x";
     const saltNonce = 42;
-    // const response =
-      await App.SimpleStorage_Module.createProxy(
+    const response = await App.ProxyFactory_Module.createProxy(
       {
         address: contractAddress,
         safeMasterCopyAddress: contractAddress,
@@ -75,8 +71,101 @@ describe("SimpleStorage", () => {
       wrapperUri
     );
 
-    // expect(response).toBeTruthy();
-    // expect(response.error).toBeFalsy();
-    // expect(response.data).not.toBeNull();
+    expect(response).toBeTruthy();
+    expect(response.error).toBeFalsy();
+    expect(response.data).not.toBeNull();
+    expect(response.data).toEqual("0xf308c38449adef77ae59b3a02b4ea1fa5d1c46e1");
+  });
+
+  it("proxyCreationCode", async () => {
+
+     const deployContractResponse = await App.Ethereum_Module.deployContract(
+       {
+       abi,
+       bytecode,
+       args: null,
+       connection: CONNECTION
+     },
+      client,
+      ethereumUri
+     );
+
+    const contractAddress = deployContractResponse.data as string;
+
+    expect(deployContractResponse).toBeTruthy();
+    expect(deployContractResponse.error).toBeFalsy();
+    expect(deployContractResponse.data).toBeTruthy();
+
+    const response = await App.ProxyFactory_Module.proxyCreationCode(
+      {
+        address: contractAddress,
+        connection: CONNECTION,
+      },
+      client,
+      wrapperUri
+    );
+
+    expect(response).toBeTruthy();
+    expect(response.error).toBeFalsy();
+    expect(response.data).not.toBeNull();
+    expect(response.data).toEqual("0x608060405234801561001057600080fd5b506040516101e63803806101e68339818101604052602081101561003357600080fd5b8101908080519060200190929190505050600073ffffffffffffffffffffffffffffffffffffffff168173ffffffffffffffffffffffffffffffffffffffff1614156100ca576040517f08c379a00000000000000000000000000000000000000000000000000000000081526004018080602001828103825260228152602001806101c46022913960400191505060405180910390fd5b806000806101000a81548173ffffffffffffffffffffffffffffffffffffffff021916908373ffffffffffffffffffffffffffffffffffffffff1602179055505060ab806101196000396000f3fe608060405273ffffffffffffffffffffffffffffffffffffffff600054167fa619486e0000000000000000000000000000000000000000000000000000000060003514156050578060005260206000f35b3660008037600080366000845af43d6000803e60008114156070573d6000fd5b3d6000f3fea26469706673582212201e33c75ec5a86b4d80c9516b3349a89d5d6c2e5232dbbbb68ec679307d136e0364736f6c63430007060033496e76616c69642073696e676c65746f6e20616464726573732070726f7669646564");
+  });
+
+  it("estimateGas", async () => {
+
+     const deployContractResponse = await App.Ethereum_Module.deployContract(
+       {
+       abi,
+       bytecode,
+       args: null,
+       connection: CONNECTION
+     },
+      client,
+      ethereumUri
+     );
+
+    const contractAddress = deployContractResponse.data as string;
+
+    expect(deployContractResponse).toBeTruthy();
+    expect(deployContractResponse.error).toBeFalsy();
+    expect(deployContractResponse.data).toBeTruthy();
+
+    const initCode = "0x";
+    const saltNonce = 42;
+    const response = await App.ProxyFactory_Module.estimateGas(
+      {
+        address: contractAddress,
+	method: "function createProxyWithNonce(address _singleton, bytes memory initializer, uint256 saltNonce)",
+	args: [contractAddress, initCode, saltNonce.toString()],
+        connection: CONNECTION,
+      },
+      client,
+      wrapperUri
+    );
+
+    expect(response).toBeTruthy();
+    expect(response.error).toBeFalsy();
+    expect(response.data).not.toBeNull();
+    expect(response.data).toEqual("113499");
+  });
+
+  it("encode", async () => {
+
+    const contractAddress = "0xf308c38449adef77ae59b3a02b4ea1fa5d1c46e1";
+    const initCode = "0x";
+    const saltNonce = 42;
+    const response = await App.ProxyFactory_Module.encode(
+      {
+	method: "function createProxyWithNonce(address _singleton, bytes memory initializer, uint256 saltNonce)",
+	args: [contractAddress, initCode, saltNonce.toString()],
+      },
+      client,
+      wrapperUri
+    );
+
+    expect(response).toBeTruthy();
+    expect(response.error).toBeFalsy();
+    expect(response.data).not.toBeNull();
+    expect(response.data).toEqual("0x1688f0b90000000000000000000000009b1f7f645351af3631a656421ed2e40f2802e6c00000000000000000000000000000000000000000000000000000000000000060000000000000000000000000000000000000000000000000000000000000002a0000000000000000000000000000000000000000000000000000000000000000");
   });
 });
--- wrapper/index.ts
+++ wrapper/index.ts
@@ -1,14 +1,45 @@
 import {
   Ethereum_Module,
-  Args_createProxy
+  Ethereum_Log,
+  Args_createProxy,
+  Args_proxyCreationCode,
+  Args_estimateGas,
+  Args_encode,
 } from "./wrap";
+import { BigInt } from "@polywrap/wasm-as";
 
-export function createProxy(args: Args_createProxy): bool {
+export function encode(args: Args_encode): string {
+  return Ethereum_Module.encodeFunction({
+    method: args.method,
+    args: args.args
+  }).unwrap()
+}
+
+export function estimateGas(args: Args_estimateGas): BigInt {
+  const res = Ethereum_Module.estimateContractCallGas({
+    address: args.address,
+    method: args.method,
+    args: args.args,
+    connection: args.connection,
+    txOverrides: null,
+  }).unwrap();
+  return res;
+}
+
+export function proxyCreationCode(args: Args_proxyCreationCode): string {
+  const res = Ethereum_Module.callContractView({
+    address: args.address,
+    method: "function proxyCreationCode() public pure returns (bytes memory)",
+    connection: args.connection,
+  }).unwrap();
+  return res;
+}
+
+export function createProxy(args: Args_createProxy): string {
   const res = Ethereum_Module.callContractMethod({
     address: args.address,
     method: "function createProxyWithNonce(address _singleton, bytes memory initializer, uint256 saltNonce)",
-    // TODO: options
-    args: [args.safeMasterCopyAddress, args.initializer, "1"],
+    args: [args.safeMasterCopyAddress, args.initializer, args.saltNonce.toString()],
     connection: args.connection,
     txOverrides: null,
   }).unwrap();
@@ -19,8 +50,14 @@ export function createProxy(args: Args_createProxy): bool {
     timeout: 60000,
   }).unwrap();
 
-  // TODO: find `ProxyCreation` event in logs
-  // const logs = tx.logs;
+  const proxyCreation = "0x4f51faf6c4561ff95f067657e43439f0f856d97c04d9ec9070a6199ad418e235";
+  //const data = tx.logs[0].topics[0];
+  const index = tx.logs.findIndex( (log: Ethereum_Log) => log.topics[0] == proxyCreation);
+  if (index == -1) {
+          return "not found"
+  }
+  const data = tx.logs[index].data;
+  const address = "0x" + tx.logs[index].data.slice(26, 66);
 
-  return true;
+  return address;
 }
