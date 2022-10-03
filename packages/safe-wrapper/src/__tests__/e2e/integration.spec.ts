0daf9b515bccf69977e7a660e6831702f5ad1062
n0cte <ilnur.a.g@gmail.com>
2022-10-03 18:27:17 +0300

    add simple tests

--- packages/safe-wrapper/src/__tests__/e2e/integration.spec.ts
+++ packages/safe-wrapper/src/__tests__/e2e/integration.spec.ts
@@ -1,30 +1,160 @@
-import { PolywrapClient } from "@polywrap/client-js";
-import * as App from "../types/wrap";
 import path from "path";
+import { PolywrapClient } from "@polywrap/client-js";
+import {
+  initTestEnvironment,
+  stopTestEnvironment,
+  providers,
+  ensAddresses,
+} from "@polywrap/test-env-js";
+import * as App from "../types/wrap";
+import { getPlugins } from "../utils";
 
-jest.setTimeout(60000);
+jest.setTimeout(1200000);
+describe("Safe Wrapper", () => {
+  const wrapper = App.SafeWrapper_Module;
+  const connection = { networkNameOrChainId: "goerli" };
+  const txOverrides = { gasLimit: "1000000", gasPrice: "20" };
+  const owners = ["0xd405aebF7b60eD2cb2Ac4497Bddd292DEe534E82"];
+  const someAddr = "0x8dc847af872947ac18d5d63fa646eb65d4d99560";
 
-describe("Template Wrapper End to End Tests", () => {
+  let client: PolywrapClient;
+  let safeAddress: string;
+  const wrapperPath: string = path.join(
+    path.resolve(__dirname),
+    "..",
+    "..",
+    ".."
+  );
+  const wrapperUri = `fs/${wrapperPath}/build`;
+  const safeWrapperPath: string = path.join(
+    path.resolve(__dirname),
+    "..",
+    "..",
+    "..",
+    "..",
+    "safe-factory-wrapper"
+  );
+  const safeWrapperUri = `fs/${safeWrapperPath}/build`;
 
-  const client: PolywrapClient = new PolywrapClient();
-  let wrapperUri: string;
+  beforeAll(async () => {
+    await initTestEnvironment();
 
-  beforeAll(() => {
-    const dirname: string = path.resolve(__dirname);
-    const wrapperPath: string = path.join(dirname, "..", "..", "..");
-    wrapperUri = `fs/${wrapperPath}/build`;
-  })
+    const plugins = getPlugins(
+      providers.ethereum,
+      ensAddresses.ensAddress,
+      connection.networkNameOrChainId
+    );
+    safeAddress = (await App.SafeFactory_Module.deploySafe(
+      {
+        safeAccountConfig: {
+          owners: owners,
+          threshold: 1,
+        },
+        connection: connection,
+        txOverrides: txOverrides,
+      },
+      new PolywrapClient(plugins),
+      safeWrapperUri
+    )).data!.safeAddress;
+    client = new PolywrapClient({
+      ...plugins,
+      envs: [{
+        uri: wrapperUri,
+        env: {
+          safeAddress: safeAddress,
+          connection: connection,
+        }
+      }]
+    })
+  });
 
-  it("calls sampleMethod", async () => {
-    const expected: string = "polywrap";
+  afterAll(async () => {
+    await stopTestEnvironment();
+  });
 
-    const { data, error } = await client.invoke<App.Template_SampleResult>({
-      uri: wrapperUri,
-      method: "sampleMethod",
-      args: { arg: expected }
+  describe("Owner Manager", () => {
+    it("getOwners", async () => {
+      const resp = await wrapper.getOwners({}, client, wrapperUri);
+      expect(resp).toBeTruthy();
+      expect(resp.error).toBeFalsy();
+      expect(resp.data).not.toBeNull();
+      expect(resp.data).toEqual(owners);
     });
 
-    expect(error).toBeFalsy();
-    expect(data?.value).toEqual(expected);
+    it("getThreshold", async () => {
+      const resp = await wrapper.getThreshold({}, client, wrapperUri);
+      expect(resp).toBeTruthy();
+      expect(resp.error).toBeFalsy();
+      expect(resp.data).not.toBeNull();
+      expect(resp.data).toEqual(1);
+    });
+
+    it("isOwner", async () => {
+      const resp = await wrapper.isOwner({ ownerAddress: owners[0] }, client, wrapperUri);
+      expect(resp).toBeTruthy();
+      expect(resp.error).toBeFalsy();
+      expect(resp.data).not.toBeNull();
+      expect(resp.data).toEqual(true);
+    });
+
+    it("encodeAddOwnerWithThresholdData", async () => {
+      const resp = await wrapper.encodeAddOwnerWithThresholdData({ ownerAddress: someAddr }, client, wrapperUri);
+      expect(resp).toBeTruthy();
+      expect(resp.error).toBeFalsy();
+      expect(resp.data).not.toBeNull();
+    });
+
+    it("encodeRemoveOwnerData", async () => {
+      const resp = await wrapper.encodeRemoveOwnerData({ ownerAddress: owners[0] }, client, wrapperUri);
+      expect(resp).toBeTruthy();
+      expect(resp.error).toBeFalsy();
+      expect(resp.data).not.toBeNull();
+    });
+
+    it("encodeSwapOwnerData", async () => {
+      const resp = await wrapper.encodeSwapOwnerData({ oldOwnerAddress: owners[0], newOwnerAddress: someAddr }, client, wrapperUri);
+      expect(resp).toBeTruthy();
+      expect(resp.error).toBeFalsy();
+      expect(resp.data).not.toBeNull();
+    });
+
+    it("encodeChangeThresholdData", async () => {
+      const resp = await wrapper.encodeChangeThresholdData({ threshold: 2 }, client, wrapperUri);
+      expect(resp).toBeTruthy();
+      expect(resp.error).toBeFalsy();
+      expect(resp.data).not.toBeNull();
+    });
+  });
+
+  describe("Module Manager", () => {
+    it("getModules", async () => {
+      const resp = await wrapper.getModules({}, client, wrapperUri);
+      expect(resp).toBeTruthy();
+      expect(resp.error).toBeFalsy();
+      expect(resp.data).not.toBeNull();
+      expect(resp.data).toEqual([]);
+    });
+
+    it("isModuleEnabled", async () => {
+      const resp = await wrapper.isModuleEnabled({ moduleAddress: someAddr }, client, wrapperUri);
+      expect(resp).toBeTruthy();
+      expect(resp.error).toBeFalsy();
+      expect(resp.data).not.toBeNull();
+      expect(resp.data).toEqual(false);
+    });
+
+    it("encodeEnableModuleData", async () => {
+      const resp = await wrapper.encodeEnableModuleData({ moduleAddress: someAddr }, client, wrapperUri);
+      expect(resp).toBeTruthy();
+      expect(resp.error).toBeFalsy();
+      expect(resp.data).not.toBeNull();
+    });
+
+    it("encodeDisableModuleData", async () => {
+      const resp = await wrapper.encodeDisableModuleData({ moduleAddress: someAddr }, client, wrapperUri);
+      expect(resp).toBeTruthy();
+      expect(resp.error).toBeFalsy();
+      expect(resp.data).not.toBeNull();
+    });
   });
 });
--- packages/safe-wrapper/src/__tests__/utils.ts
+++ packages/safe-wrapper/src/__tests__/utils.ts
@@ -1,41 +1,66 @@
 import { ClientConfig } from "@polywrap/client-js";
 import { ensResolverPlugin } from "@polywrap/ens-resolver-plugin-js";
 import { Connection, Connections, ethereumPlugin } from "@polywrap/ethereum-plugin-js";
-import { ipfsPlugin } from "@polywrap/ipfs-plugin-js";
+import { dateTimePlugin } from "polywrap-datetime-plugin";
+import { loggerPlugin } from "@polywrap/logger-plugin-js";
+import { Wallet } from "ethers";
 
 export function getPlugins(
   ethereum: string,
-  ipfs: string,
-  ensAddress: string
+  ensAddress: string,
+  network: string,
 ): Partial<ClientConfig> {
   return {
     redirects: [
       {
         from: "wrap://ens/safe-proxy-factory-wrapper.polywrap.eth",
-        to: "wrap://ipfs/QmTyaVrUsM5P9kLyK2uJ2Uvohgk2A7QSXe9M22mfaeX42D",
+        to: `wrap://ipfs/QmTyaVrUsM5P9kLyK2uJ2Uvohgk2A7QSXe9M22mfaeX42D`,
       },
+      {
+        from: "wrap://ens/safefactory.eth",
+        to: "wrap://ipfs/QmPajKxGQZwU1PW22dSEKq8eLHhmRs7rsDVraC8S1gfxda",
+      }
     ],
     plugins: [
-      {
+      /*{
         uri: "wrap://ens/ipfs.polywrap.eth",
         plugin: ipfsPlugin({ provider: ipfs }),
+      },*/
+      {
+        uri: "wrap://ens/ens.polywrap.eth",
+        plugin: ensResolverPlugin({ addresses: { testnet: ensAddress } }),
       },
       {
-        uri: "wrap://ens/ens-resolver.polywrap.eth",
-        plugin: ensResolverPlugin({ addresses: { testnet: ensAddress } }),
+        uri: "wrap://ens/datetime.polywrap.eth",
+        //@ts-ignore
+        plugin: dateTimePlugin({}),
+      },
+      {
+        uri: "wrap://ens/js-logger.polywrap.eth",
+        plugin: loggerPlugin({
+          logFunc: (level, message) => {
+            console.log(level, message);
+            return true;
+          },
+        }),
       },
       {
         uri: "wrap://ens/ethereum.polywrap.eth",
         plugin: ethereumPlugin({
           connections: new Connections({
             networks: {
+              [network]: new Connection({
+                provider: `https://${network}.infura.io/v3/9d16956e670e4429b9fc821128eb259c`, // ethereum,
+                signer: new Wallet(
+                  "8ca435f1321b8043d984d95776cf53f570f2e296f86a8b0c9ddbd7c537cee6a2"
+                ),
+              }),
               testnet: new Connection({ provider: ethereum }),
-              mainnet: new Connection({ provider: "http://localhost:8546", }),
             },
-            defaultNetwork: "testnet",
+            defaultNetwork: network,
           }),
         }),
-      },
+      }
     ],
   };
 }
