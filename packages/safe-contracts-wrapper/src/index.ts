0ec872efa877f1d5489156bb14e048ea76b664e3
Mike <mikelrap@gmail.com>
2022-11-11 11:55:57 +0200

    added get methods to safe-contracts

--- packages/safe-contracts-wrapper/src/index.ts
+++ packages/safe-contracts-wrapper/src/index.ts
@@ -14,14 +14,9 @@ import {
 } from "./wrap";
 import { BigInt } from "@polywrap/wasm-as";
 import { JSON } from "assemblyscript-json";
-import { Args_getSafeContractNetworks } from "./wrap/Module/serialization";
+import { Args_approvedHashes, Args_getAddress, Args_getNonce, Args_getSafeContractNetworks, Args_getVersion } from "./wrap/Module/serialization";
 import { ContractNetworksConfig } from "./wrap/ContractNetworksConfig";
-import {
-  getMultiSendCallOnlyContractAddress,
-  getMultiSendContractAddress,
-  getSafeContractAddress,
-  getSafeFactoryContractAddress,
-} from "./utils";
+import { getMultiSendCallOnlyContractAddress, getMultiSendContractAddress, getSafeContractAddress, getSafeFactoryContractAddress } from "./utils";
 
 export function encode(args: Args_encode): string {
   return Ethereum_Module.encodeFunction({
@@ -62,9 +57,7 @@ export function createProxy(args: Args_createProxy): string | null {
   const proxyCreation_1_2_0 = "0xa38789425dbeee0239e16ff2d2567e31720127fbc6430758c1a4efc6aef29f80";
   // ProxyCreation(address,address)
   const proxyCreation_1_3_0 = "0x4f51faf6c4561ff95f067657e43439f0f856d97c04d9ec9070a6199ad418e235";
-  const index = tx.logs.findIndex(
-    (log: Ethereum_Log) => log.topics[0] == proxyCreation_1_2_0 || log.topics[0] == proxyCreation_1_3_0
-  );
+  const index = tx.logs.findIndex((log: Ethereum_Log) => log.topics[0] == proxyCreation_1_2_0 || log.topics[0] == proxyCreation_1_3_0);
 
   if (index == -1) {
     return null;
@@ -72,6 +65,46 @@ export function createProxy(args: Args_createProxy): string | null {
   const address = "0x" + tx.logs[index].data.slice(26, 66);
 
   return address;
+}
+
+/*--------------
+  GnosisSafeContractEthers methods
+--------------*/
+
+export function getVersion(args: Args_getVersion): string {
+  const version = Ethereum_Module.callContractView({
+    address: args.address,
+    method: "function VERSION() public view returns (string)",
+    args: [],
+    connection: args.connection,
+  }).unwrap();
+
+  return version;
+}
+
+export function getAddress(args: Args_getAddress): string {
+  return args.address;
+}
+
+export function getNonce(args: Args_getNonce): BigInt {
+  const nonce = Ethereum_Module.callContractView({
+    address: args.address,
+    method: "function nonce() public view returns (uint256)",
+    args: [],
+    connection: args.connection,
+  }).unwrap();
+
+  return BigInt.from(nonce);
+}
+
+export function getThreshold(args: Args_getThreshold): u32 {
+  const resp = Ethereum_Module.callContractView({
+    address: args.address,
+    method: "function getThreshold() public view returns (uint256)",
+    args: null,
+    connection: args.connection,
+  }).unwrap();
+  return u32(parseInt(resp, 10));
 }
 
 export function getOwners(args: Args_getOwners): string[] {
@@ -98,16 +131,6 @@ export function getOwners(args: Args_getOwners): string[] {
   return result;
 }
 
-export function getThreshold(args: Args_getThreshold): u32 {
-  const resp = Ethereum_Module.callContractView({
-    address: args.address,
-    method: "function getThreshold() public view returns (uint256)",
-    args: null,
-    connection: args.connection,
-  }).unwrap();
-  return u32(parseInt(resp, 10));
-}
-
 export function isOwner(args: Args_isOwner): bool {
   const resp = Ethereum_Module.callContractView({
     address: args.address,
@@ -122,11 +145,20 @@ export function isOwner(args: Args_isOwner): bool {
   }
 }
 
+export function approvedHashes(args: Args_approvedHashes): BigInt {
+  const result = Ethereum_Module.callContractView({
+    address: args.address,
+    method: "function approvedHashes(address owner, bytes32 hash) public view returns (uint256)",
+    args: [args.ownerAddress, args.hash],
+    connection: args.connection,
+  }).unwrap();
+  return BigInt.from(result);
+}
+
 export function getModules(args: Args_getModules): string[] {
   const resp = Ethereum_Module.callContractView({
     address: args.address,
-    method:
-      "function getModulesPaginated(address start, uint256 pageSize) external view returns (address[] memory array, address next)",
+    method: "function getModulesPaginated(address start, uint256 pageSize) external view returns (address[] memory array, address next)",
     args: ["0x0000000000000000000000000000000000000001", "0xa"],
     connection: args.connection,
   }).unwrap();
